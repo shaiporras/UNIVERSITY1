47e01201177e9339a8e4757b10608d259e7bf36e
shainaporras <119840735+shainaporras@users.noreply.github.com>
2023-06-07 10:05:29 +0800

    htaccess and path rewrite

--- session.php
+++ session.php
@@ -1,15 +1,13 @@
 <?php
 include('config.php');
 session_start();
-$email_check=$_SESSION['email'];
-$ses_sql=mysqli_query($con,"select email,id from createuser where email='$email_check'");
-$row=mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-$loggedin_session=$row['email'];
-$loggedin_id=$row['id'];
-if(!isset($loggedin_session) || $loggedin_session==NULL) {
+$email_check = $_SESSION['email'];
+$ses_sql = mysqli_query($con, "select email,id from createuser where email='$email_check'");
+$row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
+$loggedin_session = $row['email'];
+$loggedin_id = $row['id'];
+if (!isset($loggedin_session) || $loggedin_session == NULL) {
 	echo "Go back";
 	header("Location: login.php");
 }
 ?>
-
-
